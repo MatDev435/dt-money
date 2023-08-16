3bea8e0e8d8714e8a77dee283de47316523e1c7d
MatDev435 <matdev435@gmail.com>
2023-08-16 11:51:03 -0300

    Componente modal criado

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,39 @@
+import * as Dialog from '@radix-ui/react-dialog'
+import { Close, Content, Overlay, TransactionType, TransactionTypeButton } from './styles'
+import { X, ArrowCircleUp, ArrowCircleDown } from 'phosphor-react'
+
+export function NewTransactionModal() {
+    return (
+        <Dialog.Portal>
+            <Overlay />
+
+            <Content>
+                <Dialog.Title>Nova transação</Dialog.Title>
+
+                <Close>
+                    <X size={24} />
+                </Close>
+
+                <form action="">
+                    <input type="text" placeholder='Descrição' required />
+                    <input type="number" placeholder='Preço' required />
+                    <input type="text" placeholder='Categoria' required />
+
+                    <TransactionType>
+                        <TransactionTypeButton variant='income'>
+                            <ArrowCircleUp size={24} />
+                            Entrada
+                        </TransactionTypeButton>
+
+                        <TransactionTypeButton variant='outcome'>
+                            <ArrowCircleDown size={24} />
+                            Saída
+                        </TransactionTypeButton>
+                    </TransactionType>
+
+                    <button type='submit'>Cadastrar</button>
+                </form>
+            </Content>
+        </Dialog.Portal>
+    )
+}
